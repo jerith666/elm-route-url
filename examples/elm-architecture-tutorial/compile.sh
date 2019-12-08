dcda975c9e5fea9fa94480ebc66590a8a595c1ba
Matt McHenry <github@matt.mchenryfamily.org>
2019-12-07 19:28:02 -0500

    remove old api from compile.sh

--- examples/elm-architecture-tutorial/compile.sh
+++ examples/elm-architecture-tutorial/compile.sh
@@ -3,4 +3,3 @@
 mkdir -p build
 elm make MainWithFullUrl.elm --output build/MainWithFullUrl.html
 elm make MainWithJustHash.elm --output build/MainWithJustHash.html
-elm make MainWithOldAPI.elm --output build/MainWithOldAPI.html
