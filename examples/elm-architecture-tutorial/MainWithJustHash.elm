e87ff3f1a1a5da30cd8f8a7becc50b5359a3506e
Matt McHenry <github@matt.mchenryfamily.org>
2019-12-07 19:28:34 -0500

    fixes to MainWithJustHash

--- examples/elm-architecture-tutorial/MainWithJustHash.elm
+++ examples/elm-architecture-tutorial/MainWithJustHash.elm
@@ -1,16 +1,17 @@
 module MainWithJustHash exposing (..)
 
-import ExampleViewer exposing (Action, Model)
+import ExampleViewer exposing (Action(..), Model)
 import RouteUrl exposing (RouteUrlProgram)
 
 
-main : RouteUrlProgram Never Model Action
+main : RouteUrlProgram () Model Action
 main =
-    RouteUrl.program
+    RouteUrl.programWithFlags
         { delta2url = ExampleViewer.delta2hash
         , location2messages = ExampleViewer.hash2messages
         , init = ExampleViewer.init
         , update = ExampleViewer.update
         , view = ExampleViewer.view
         , subscriptions = ExampleViewer.subscriptions
+        , onUrlRequest = UrlRequested
         }
